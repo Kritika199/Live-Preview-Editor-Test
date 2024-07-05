cb4294fd4452bc09d9568ff271653451e4006a48
Kritika199 <68268954+Kritika199@users.noreply.github.com>
2024-07-05 14:29:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Salesforce Marketing Cloud SDK Initialization
-var SDK = function (config, whitelistOverride, sslOverride) {
+var SDK = function(config, whitelistOverride, sslOverride) {
     if (Array.isArray(config)) {
         whitelistOverride = config;
         sslOverride = whitelistOverride;
@@ -16,9 +16,7 @@ var SDK = function (config, whitelistOverride, sslOverride) {
     this._whitelistOverride = whitelistOverride;
     this._sslOverride = sslOverride;
     this._messageId = 1;
-    this._messages = {
-        0: function () {}
-    };
+    this._messages = {};
     this._readyToPost = false;
     this._pendingMessages = [];
     this._receiveMessage = this._receiveMessage.bind(this);
@@ -32,7 +30,7 @@ var SDK = function (config, whitelistOverride, sslOverride) {
     }, '*');
 };
 
-SDK.prototype.execute = function execute(method, options) {
+SDK.prototype.execute = function(method, options) {
     options = options || {};
 
     var self = this;
@@ -53,80 +51,80 @@ SDK.prototype.execute = function execute(method, options) {
     }
 };
 
-SDK.prototype.getCentralData = function (cb) {
+SDK.prototype.getCentralData = function(cb) {
     this.execute('getCentralData', {
         success: cb
     });
 };
 
-SDK.prototype.getContent = function (cb) {
+SDK.prototype.getContent = function(cb) {
     this.execute('getContent', {
         success: cb
     });
 };
 
-SDK.prototype.getData = function (cb) {
+SDK.prototype.getData = function(cb) {
     this.execute('getData', {
         success: cb
     });
 };
 
-SDK.prototype.getUserData = function (cb) {
+SDK.prototype.getUserData = function(cb) {
     this.execute('getUserData', {
         success: cb
     });
 };
 
-SDK.prototype.getView = function (cb) {
+SDK.prototype.getView = function(cb) {
     this.execute('getView', {
         success: cb
     });
 };
 
-SDK.prototype.setBlockEditorWidth = function (value, cb) {
+SDK.prototype.setBlockEditorWidth = function(value, cb) {
     this.execute('setBlockEditorWidth', {
         data: value,
         success: cb
     });
 };
 
-SDK.prototype.setCentralData = function (dataObj, cb) {
+SDK.prototype.setCentralData = function(dataObj, cb) {
     this.execute('setCentralData', {
         data: dataObj,
         success: cb
     });
 };
 
-SDK.prototype.setContent = function (content, cb) {
+SDK.prototype.setContent = function(content, cb) {
     this.execute('setContent', {
         data: content,
         success: cb
     });
 };
 
-SDK.prototype.setData = function (dataObj, cb) {
+SDK.prototype.setData = function(dataObj, cb) {
     this.execute('setData', {
         data: dataObj,
         success: cb
     });
 };
 
-SDK.prototype.setSuperContent = function (content, cb) {
+SDK.prototype.setSuperContent = function(content, cb) {
     this.execute('setSuperContent', {
         data: content,
         success: cb
     });
 };
 
-SDK.prototype.triggerAuth = function (appID) {
-    this.getUserData(function (userData) {
+SDK.prototype.triggerAuth = function(appID) {
+    this.getUserData(function(userData) {
         var stack = userData.stack;
         if (stack.indexOf('qa') === 0) {
-            stack = stack.substring(3,5) + '.' + stack.substring(0,3);
+            stack = stack.substring(3, 5) + '.' + stack.substring(0, 3);
         }
         var iframe = document.createElement('IFRAME');
         iframe.src = 'https://mc.' + stack + '.exacttarget.com/cloud/tools/SSO.aspx?appId=' + appID + '&restToken=1&hub=1';
-        iframe.style.width= '1px';
+        iframe.style.width = '1px';
         iframe.style.height = '1px';
         iframe.style.position = 'absolute';
         iframe.style.top = '0';
@@ -137,18 +135,18 @@ SDK.prototype.triggerAuth = function (appID) {
     });
 };
 
-SDK.prototype.triggerAuth2 = function (authInfo) {
+SDK.prototype.triggerAuth2 = function(authInfo) {
     var iframe = document.createElement('IFRAME');
     var scope = '';
     var state = '';
-    if(Array.isArray(authInfo.scope)) {
+    if (Array.isArray(authInfo.scope)) {
         scope = '&scope=' + authInfo.scope.join('%20');
     }
-    if(authInfo.state) {
+    if (authInfo.state) {
         state = '&state=' + authInfo.state;
     }
-    iframe.src = authInfo.authURL + (authInfo.authURL.endsWith('/') ? '':'/') + 'v2/authorize?response_type=code&client_id=' + authInfo.clientId + '&redirect_uri=' + encodeURIComponent(authInfo.redirectURL) + scope + state;
-    iframe.style.width= '1px';
+    iframe.src = authInfo.authURL + (authInfo.authURL.endsWith('/') ? '' : '/') + 'v2/authorize?response_type=code&client_id=' + authInfo.clientId + '&redirect_uri=' + encodeURIComponent(authInfo.redirectURL) + scope + state;
+    iframe.style.width = '1px';
     iframe.style.height = '1px';
     iframe.style.position = 'absolute';
     iframe.style.top = '0';
@@ -158,12 +156,12 @@ SDK.prototype.triggerAuth2 = function (authInfo) {
     document.body.appendChild(iframe);
 };
 
-/* Internal Methods */
+// Internal Methods
 
-SDK.prototype._executePendingMessages = function _executePendingMessages () {
+SDK.prototype._executePendingMessages = function() {
     var self = this;
 
-    this._pendingMessages.forEach(function (thisMessage) {
+    this._pendingMessages.forEach(function(thisMessage) {
         self.execute(thisMessage.method, {
             data: thisMessage.payload,
             success: thisMessage.callback
@@ -173,15 +171,15 @@ SDK.prototype._executePendingMessages = function _executePendingMessages () {
     this._pendingMessages = [];
 };
 
-SDK.prototype._post = function _post (payload, callback) {
+SDK.prototype._post = function(payload, callback) {
     this._messages[this._messageId] = callback;
     payload.id = this._messageId;
-    this._messageId += 1;
     // the actual postMessage always uses the validated origin
     window.parent.postMessage(payload, this._parentOrigin);
+    this._messageId += 1;
 };
 
-SDK.prototype._receiveMessage = function _receiveMessage (message) {
+SDK.prototype._receiveMessage = function(message) {
     message = message || {};
     var data = message.data || {};
 
@@ -194,7 +192,7 @@ SDK.prototype._receiveMessage = function _receiveMessage (message) {
         }
     } else if (data.method === 'closeBlock') {
         if (this._validateOrigin(data.origin)) {
-            // here execute the method before closing the block editing
+            // execute the method before closing the block editing
             if (this.handlers && this.handlers.onEditClose) {
                 this.handlers.onEditClose();
             }
@@ -203,22 +201,22 @@ SDK.prototype._receiveMessage = function _receiveMessage (message) {
         }
     }
 
-    // if the message is not from the validated origin it gets ignored
+    // ignore messages not from the validated origin
     if (!this._parentOrigin || this._parentOrigin !== message.origin) {
         return;
     }
-    // when the message has been received, we execute its callback
-    (this._messages[data.id || 0] || function () {})(data.payload);
+    // execute the callback of received message
+    (this._messages[data.id || 0] || function() {})(data.payload);
     delete this._messages[data.id];
 };
 
-// the custom block should verify it is being called from the marketing cloud
-SDK.prototype._validateOrigin = function _validateOrigin (origin) {
-    // Make sure to escape periods since these strings are used in a regular expression
+// verify that the custom block is called from the marketing cloud
+SDK.prototype._validateOrigin = function(origin) {
+    // escape periods for the strings in regular expressions
     var allowedDomains = this._whitelistOverride || ['exacttarget\\.com', 'marketingcloudapps\\.com', 'blocktester\\.herokuapp\\.com'];
 
     for (var i = 0; i < allowedDomains.length; i++) {
-        // Makes the s optional in https
+        // optional 's' in https
         var optionalSsl = this._sslOverride ? '?' : '';
         var mcSubdomain = allowedDomains[i] === 'exacttarget\\.com' ? 'mc\\.' : '';
         var whitelistRegex = new RegExp('^https' + optionalSsl + '://' + mcSubdomain + '([a-zA-Z0-9-]+\\.)*' + allowedDomains[i] + '(:[0-9]+)?$', 'i');
@@ -234,7 +232,6 @@ SDK.prototype._validateOrigin = function _validateOrigin (origin) {
 if (typeof(window) === 'object') {
     window.sfdc = window.sfdc || {};
     window.sfdc.BlockSDK = SDK;
-     
 
     // Example usage with additional functions
 
@@ -257,16 +254,22 @@ if (typeof(window) === 'object') {
         const richTextField = document.getElementById("richTextField").contentWindow.document;
         richTextField.designMode = "on";
 
-        // Load the initial content from Salesforce Marketing Cloud
+        // Load initial content from Salesforce Marketing Cloud
         sdk.getContent(function(content) {
             richTextField.open();
             richTextField.write(content || '');
             richTextField.close();
-    sdk.setContent(content, function(getContent) {
-            // Set the initial content as the super content for preview
+
+            // Set initial content as super content for preview
             sdk.setSuperContent(content, function(newSuperContent) {
                 console.log('Super Content set:', newSuperContent);
             });
+        });
+
+        // Listen for changes in rich text editor content
+        richTextField.addEventListener('input', function() {
+            // Update content in Salesforce Marketing Cloud
+            updateContent();
         });
     }
 
@@ -275,7 +278,7 @@ if (typeof(window) === 'object') {
         const richTextField = document.getElementById("richTextField").contentWindow.document;
         richTextField.execCommand(command, false, null);
 
-        // Update the content in Salesforce Marketing Cloud
+        // Update content in Salesforce Marketing Cloud
         updateContent();
     }
 
@@ -283,7 +286,7 @@ if (typeof(window) === 'object') {
         const richTextField = document.getElementById("richTextField").contentWindow.document;
         richTextField.execCommand(command, false, value);
 
-        // Update the content in Salesforce Marketing Cloud
+        // Update content in Salesforce Marketing Cloud
         updateContent();
     }
 
@@ -299,12 +302,12 @@ if (typeof(window) === 'object') {
         });
     }
 
-    // Initialize the editor when the document is ready
+    // Initialize editor when document is ready
     document.addEventListener('DOMContentLoaded', function() {
         enableEditMode();
     });
-}
-// Set Super Content for Preview
+
+    // Set Super Content for Preview
     sdk.setSuperContent('Preview Content', function(newSuperContent) {
         console.log('New Super Content:', newSuperContent);
     });
@@ -326,9 +329,6 @@ if (typeof(window) === 'object') {
     });
 }
 
-
 if (typeof(module) === 'object') {
     module.exports = SDK;
 }
-
-
